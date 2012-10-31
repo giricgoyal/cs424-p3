1668334aa844252eac6dfaa616d2cc917c1bf02d
giric goyal <giricgoyal@gmail.com>
2012-10-31 01:11:25 -0500

    pop up and marker
    need to test, i didnt have the database, so couldnt connec tot othe bd
    and hence couldnt run the most recent code. It ran on the previous
    version of the code though.
    Need some optimization like the boundary checks while creating the
    popup.
    Working on that.

--- src/main/Marker.java
+++ src/main/Marker.java
@@ -1,6 +1,14 @@
+/**
+ * Class to create markers on the map
+ * @author giric
+ */
+
+
 package main;
 
 import processing.core.PApplet;
+import processing.core.PConstants;
+import processing.core.PShape;
 
 import com.modestmaps.geo.Location;
 
@@ -9,6 +17,54 @@ public class Marker{
 	//Needs: Shape, color, x, y, coordX, coordY
 	public Location location;
 	public PApplet parent;
+	
+	float coordX;
+	float coordY;
+	public PShape shape;
+	public PShape layer1;
+	int color;
+	
+	
+	
+	public Marker(PApplet p, float x, float y, int color) {
+		// TODO Auto-generated constructor stub	
+		this.parent = p;
+		this.color = color;
+		this.coordX = x;
+		this.coordY = y;
+		shape = parent.loadShape("marker.svg");			//for createShape1
+		layer1 = shape.getChild("svg_4");				//for createShape1
+	}
+	
+	
+	private void createShape1() {
+		shape.disableStyle();
+		parent.fill(color);
+		parent.shapeMode(PConstants.CORNER);
+		parent.shape(shape, coordX - shape.width/50,coordY - shape.height/50, shape.width/50, shape.height/50);
+		parent.fill(0);
+		parent.shape(layer1, coordX - shape.width/50,coordY - shape.height/50, layer1.width/50, layer1.height/50);
+	}
+
+	public void draw() {
+		// TODO Auto-generated method stub
+		createShape1();
+	}
+	
+	public float getWidth() {
+		return shape.width/50;
+	}
+	
+	public float getHeight() {
+		return shape.height/50;
+	}
+	
+	/**
+	 * diego's trial code, you might need to comment out later
+	 * @param x
+	 * @param y
+	 */
+	
 	public void draw(float x, float y) {
 		parent.fill(0xFF0000FF);
 		parent.noStroke();
@@ -16,4 +72,5 @@ public class Marker{
 	}
 	
 	public Marker(PApplet p, Location l) {this.parent=p; this.location=l;}
+	
 }
