fd7fd7c9e803808fd872382f722861e6005d71f3
Akrome <diego.martinoia@gmail.com>
2012-10-31 17:34:48 -0500

    other

---
+++ src/main/Positions.java
@@ -0,0 +1,9 @@
+package main;
+
+public class Positions {
+	
+	public static float keyboardX = Utilities.width*0.8f;
+	public static float keyboardY = Utilities.height*0.6f;
+	public static float keyboardWidth = Utilities.width*0.15f;
+	public static float keyboardHeight = Utilities.height*0.15f;
+}
